545a416816eb41d531fdf57a4685f582f96f7466
brendamesias <brendamesiasvillar@gmail.com>
2017-11-20 17:49:42 -0500

    cambiando el nombre de la variable boton por btn

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var characters = document.getElementById('insert');
-var boton = document.getElementById('save');
+var btn = document.getElementById('save');
 var tweetsPublicados = document.getElementById('container-tweets');
 //  creando función que cuenta n° de caracteres ingresados
 characters.addEventListener('keyup', function(event) {
@@ -7,29 +7,29 @@ characters.addEventListener('keyup', function(event) {
   var size = characters.value.length;
   var max = 140;
   var finalSize = numberCharacters.textContent = (max - size);
-  // si los caracteres pasan los 140, desabilitar el boton
+  // si los caracteres pasan los 140, desabilitar el btn
   if (finalSize <= 0) {
-    boton.disabled = true;
+    btn.disabled = true;
     numberCharacters.style.color = 'red';
 
     // cambiar contador a color morado si hay más de 120 caracteres
   } else if (finalSize <= 20 && finalSize > 10) {
-    boton.disabled = false;
+    btn.disabled = false;
     numberCharacters.style.color = 'purple';
     // cambiar contador a color fucsia si hay más de 130 caracteres
   } else if (finalSize < 10 && finalSize >= 0) {
-    boton.disabled = false;
+    btn.disabled = false;
     numberCharacters.style.color = '#FF1493';
   } else if (finalSize < 0) {
-    boton.disabled = false;
+    btn.disabled = false;
     numberCharacters.style.color = 'red';
   } else {
-    boton.disabled = false;
+    btn.disabled = false;
     numberCharacters.style.color = '#7788' ;
   }
 });
 // creando función que allacene lo que es cribe en el textArea para que lo muestre como tweets en un parrafo
-boton.addEventListener('click', function(event) {
+btn.addEventListener('click', function(event) {
   if (characters.value) {
     var tweet = document.createElement('p');
     var textTweet = document.createTextNode('');
